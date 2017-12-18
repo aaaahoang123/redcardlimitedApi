995724f27d0dfd205a69d79e156b34af38f6d5fc
Hoang <shinkenger.vn9x@gmail.com>
2017-12-18 18:24:45 +0700

    fixed connect

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     cors = require('cors');
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb://aaaahoang123:q07cmf4gt1kb@cluster0-shard-00-00-5tj8j.mongodb.net:27017,cluster0-shard-00-01-5tj8j.mongodb.net:27017,cluster0-shard-00-02-5tj8j.mongodb.net:27017/rclapi?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+MongoClient.connect(uri, function(err, db) {
+    db.close();
+});
 
 var user = require('./api/routes/userRoute'),
     authentication = require('./api/routes/authenticationRoute');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://aaaahoang123:q07cmf4gt1kb@cluster0-shard-00-00-5tj8j.mongodb.net:27017,cluster0-shard-00-01-5tj8j.mongodb.net:27017,cluster0-shard-00-02-5tj8j.mongodb.net:27017/test?ssl=true&authSource=admin');
+//mongoose.connect('mongodb://aaaahoang123:q07cmf4gt1kb@cluster0-shard-00-00-5tj8j.mongodb.net:27017,cluster0-shard-00-01-5tj8j.mongodb.net:27017,cluster0-shard-00-02-5tj8j.mongodb.net:27017/rclapi?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 // cors to allow post, put method
 app.use(cors());
