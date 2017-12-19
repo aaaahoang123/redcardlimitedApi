099636185b3ed24fed32db291fdfc8c9a48125c8
Hoang <shinkenger.vn9x@gmail.com>
2017-12-19 21:39:51 +0700

    product controller updated

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -6,23 +6,23 @@ const allModels = require('../models/productModels'),
     occasionModel = allModels.occasionModel;
 
 module.exports = {
-  getAll: function (req, res, next) {
-    productModel.find({}, function (err, result) {
-       if (err) {
-           console.log(err);
-           res.status(500);
+    getAll: function (req, res, next) {
+        productModel.find({}, function (err, result) {
+           if (err) {
+               console.log(err);
+               res.status(500);
+               res.send({
+                   error: 'Server error, please contact us'
+               });
+               return;
+           }
            res.send({
-               error: 'Server error, please contact us'
+               'status': '200',
+               'attributes': result
            });
-           return;
-       }
-       res.send({
-           'status': '200',
-           'attributes': result
-       });
-    });
-  },
-  getById: function (req, res, next) {
+        });
+    },
+    getById: function (req, res, next) {
       productModel.find({_id: req.params.id}, function (err, result) {
          if (err) {
              console.log(err);
@@ -45,5 +45,53 @@ module.exports = {
              'item': result[0]
          });
       });
-  },
+    },
+    getByBrandId: function (req, res, next) {
+      productModel.find({brandId: req.query.brandId}, function (err, result) {
+         if (err) {
+             console.log(err);
+             res.status(500);
+             res.send({
+                 error: 'Server error, please contact us'
+             });
+             return;
+         }
+         else if (result.length === 0) {
+             res.status = 404;
+             res.send({
+                 error: '404',
+                 message: 'Not found'
+             });
+             return;
+         }
+          res.send({
+              'status': '200',
+              'item': result[0]
+          });
+      });
+    },
+    getByOccasionId: function (req, res, next) {
+        productModel.find({occasionId: req.query.occasionId}, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+                res.send({
+                    error: 'Server error, please contact us'
+                });
+                return;
+            }
+            else if (result.length === 0) {
+                res.status = 404;
+                res.send({
+                    error: '404',
+                    message: 'Not found'
+                });
+                return;
+            }
+            res.send({
+                'status': '200',
+                'item': result[0]
+            });
+        });
+    }
 };
